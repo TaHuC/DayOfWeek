3ebf9007ba0b0ced6b1e68fc75d0f6cf12f97c2e
TaHuC <TaHuC@hotmail.com>
2017-02-03 22:34:45 +0200

    Finish Day of week

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DayOFWeek
+{
+	class MainClass
+	{
+		public static void Main()
+		{
+			string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+			var arrayNum = int.Parse(Console.ReadLine());
+
+			if (arrayNum <= 0 || arrayNum > 7)
+			{
+				Console.WriteLine("Invalid Day!");
+				return;
+			}
+
+			for (var i = 0; i <= days.Length; i++)
+			{
+				if (i == arrayNum )
+				{
+					Console.WriteLine(days[i - 1]);
+				}
+			}
+		}
+	}
+}
